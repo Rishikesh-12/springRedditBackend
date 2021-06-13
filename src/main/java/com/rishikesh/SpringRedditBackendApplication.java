e1d9483425ede887aeb5b9b5bdd8090612b11332
Rishikesh <contactrishikeshhere@gmail.com>
2021-06-13 20:24:05 +0530

    initialized repository

---
+++ src/main/java/com/rishikesh/SpringRedditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rishikesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedditBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishikesh/SpringRedditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishikesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
